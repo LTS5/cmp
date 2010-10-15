64f0db4c06e4f3266106acf79afe2640aa647cdb
Stephan Gerhard <git@unidesign.ch>
2010-10-15 19:11:01 +0200

    Added template module folder

---
+++ cmt/modules/template_module/modulename.py
@@ -0,0 +1,34 @@
+""" This is a template module to describe the basic scaffold for new modules """
+
+import os, os.path as op
+import sys
+from time import time
+from ...logme import *
+
+def process_function():
+    
+    print "Here you define your processing ..."
+
+def run(conf):
+    """ Run the NONAME module
+    
+    Parameters
+    ----------
+    conf : PipelineConfiguration object
+        
+    """
+    # setting the global configuration variable
+    globals()['gconf'] = conf
+    globals()['log'] = gconf.get_logger() 
+    start = time()
+    
+    log.info("NONAME MODULE")
+    log.info("=============")
+
+    process_function()
+    
+    log.info("Module took %s seconds to process." % (time()-start))
+    
+    if not len(gconf.emailnotify) == 0:
+        msg = "NONAME module finished!\nIt took %s seconds." % int(time()-start)
+        send_email_notification(msg, gconf.emailnotify, log)
