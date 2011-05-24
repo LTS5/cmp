6d023de0a580b08ef494c2f2f42e90df6071df97
Stephan Gerhard <git@unidesign.ch>
2011-05-24 17:39:26 +0200

    reordering of parcellation labels, scale 1015

---
+++ scratch/reorder_labels_parcellation1015.py
@@ -0,0 +1,18 @@
+b = nx.Graph() 
+for i,elem in enumerate(right):
+	id = str(i+1)
+	for idn,dictn in a.nodes(data=True):
+		if dictn['dn_fsname'] == elem:
+			b.add_node(id,dictn)
+			b.node[id]['dn_correspondence_id'] = id
+for i,elem in enumerate(left):
+	id = str(i+509) # skip the subcortical region: 501 + 7 + 1
+	for idn,dictn in a.nodes(data=True):
+		if dictn['dn_fsname'] == elem:
+			b.add_node(id,dictn)
+			b.node[id]['dn_correspondence_id'] = id
+for i,dictn in a.nodes(data=True):
+	if dictn['dn_region'] == 'subcortical':
+		b.add_node(i,dictn)
+
+
