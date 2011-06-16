60c282233f3e7a0667c3c767fdd72a5702e17ace
Alessandra Griffa <alessandra.griffa@epfl.ch>
2011-06-16 18:08:16 +0200

    reordering parcellation labels for all resolutions

--- scratch/reorder_labels_parcellation1015.py
+++ scratch/reorder_labels_parcellation1015.py
@@ -2,13 +2,13 @@ b = nx.Graph()
 for i,elem in enumerate(right):
 	id = str(i+1)
 	for idn,dictn in a.nodes(data=True):
-		if dictn['dn_fsname'] == elem and dictn['dn_hemisphere'] == 'right':
+		if dictn['dn_fsname'] == elem and dictn['dn_hemisphere'] == 'right' and dictn['dn_region'] == 'cortical':
 			b.add_node(id,dictn)
 			b.node[id]['dn_correspondence_id'] = id
 for i,elem in enumerate(left):
 	id = str(i+509) # skip the subcortical region: 501 + 7 + 1
 	for idn,dictn in a.nodes(data=True):
-		if dictn['dn_fsname'] == elem and dictn['dn_hemisphere'] == 'left':
+		if dictn['dn_fsname'] == elem and dictn['dn_hemisphere'] == 'left' and dictn['dn_region'] == 'cortical':
 			b.add_node(id,dictn)
 			b.node[id]['dn_correspondence_id'] = id
 for i,dictn in a.nodes(data=True):
