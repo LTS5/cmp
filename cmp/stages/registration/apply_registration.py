59392e2b79b6903323fce5eb392c624699342d38
Alessandra Griffa <alessandra.griffa@epfl.ch>
2012-01-25 10:53:29 +0100

    fix bug apply_registration for release 1.2.0

--- cmp/stages/registration/apply_registration.py
+++ cmp/stages/registration/apply_registration.py
@@ -40,6 +40,8 @@ def apply_nlin_registration():
     warp_files = ['fsmask_1mm.nii.gz']
     for park in gconf.parcellation.keys():
         warp_files.append(op.join(park, 'ROI_HR_th.nii.gz'))
+        if gconf.parcellation_scheme == "Lausanne2008":
+            warp_files.append(op.join(park, 'ROIv_HR_th.nii.gz'))
     
     for infile in warp_files:
         log.info("Warp file: %s" % infile)
@@ -91,7 +93,8 @@ def apply_lin_registration():
     warp_files = ['fsmask_1mm.nii.gz']
     for park in gconf.parcellation.keys():
         warp_files.append(op.join(park, 'ROI_HR_th.nii.gz'))
-        warp_files.append(op.join(park, 'ROIv_HR_th.nii.gz'))
+        if gconf.parcellation_scheme == "Lausanne2008":
+            warp_files.append(op.join(park, 'ROIv_HR_th.nii.gz'))
     
     for infile in warp_files:
         log.info("Warp file: %s" % infile)
@@ -145,16 +148,19 @@ def declare_inputs(conf):
     """Declare the inputs to the stage to the PipelineStatus object"""
     
     stage = conf.pipeline_status.GetStage(__name__)
-    nifti_trafo_dir = conf.get_nifti_trafo()    
+    nifti_trafo_dir = conf.get_nifti_trafo()
+    nifti_dir = conf.get_nifti()
     tracto_masks_path = conf.get_cmp_tracto_mask()
     
     if conf.registration_mode == 'Nonlinear':
         conf.pipeline_status.AddStageInput(stage, nifti_trafo_dir, 'T1-TO-T2.mat', 'T1-TO-T2-mat')
+        conf.pipeline_status.AddStageInput(stage, nifti_dir, 'T2-TO-b0_warp.nii.gz', 'T2-TO-b0_warp-nii-gz')
     
     elif conf.registration_mode == 'Linear':
         conf.pipeline_status.AddStageInput(stage, nifti_trafo_dir, 'T1-TO-b0.mat', 'T1-TO-b0-mat')
         
-    conf.pipeline_status.AddStageInput(stage, tracto_masks_path, 'fsmask_1mm.nii.gz', 'fsmask_1mm-nii-gz')        
+    conf.pipeline_status.AddStageInput(stage, tracto_masks_path, 'fsmask_1mm.nii.gz', 'fsmask_1mm-nii-gz')
+    conf.pipeline_status.AddStageInput(stage, nifti_dir, 'Diffusion_b0_resampled.nii.gz', 'Diffusion_b0_resampled-nii-gz')
     for p in conf.parcellation.keys():
         conf.pipeline_status.AddStageInput(stage, op.join(tracto_masks_path, p), 'ROI_HR_th.nii.gz', 'ROI_HR_th_%s-nii-gz' % (p))
 
