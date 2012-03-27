aea1c9c84a22b2a3c49016ce5784a969b5692102
Nicolas Rannou <huko.rannou@gmail.com>
2012-03-27 15:12:35 -0400

    WIP: ubuntu12.04

--- cmp/connectome.py
+++ cmp/connectome.py
@@ -57,10 +57,16 @@ def mapit(cobj):
     # Execute the pipeline
     for stage, stageEnabled in stages:
         if stageEnabled == True:
+            print '--> running: ' + stage.__name__
             curStageObj = cobj.pipeline_status.GetStage( stage.__name__ )
 
             # Check if the inputs exist
             if curStageObj != None:
+
+                for curInput in curStageObj.inputs:
+                  filePath = op.join(curInput.rootDir, curInput.filePath)
+                  print 'requiered filePath: ' + filePath
+
                 if cobj.pipeline_status.CanRun( curStageObj ) == False:
                     # TODO: depending on the stage's missing file, point to required stages
                     msg = "Required input file missing for stage: '%s'. Please run all previous stages." % (stage.__name__)
